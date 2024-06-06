fd0fb75dea3bc4b7abe2a2be70995e5a812ae685
m4Fagundes <matheusfagundesaraujo@gmail.com>
2024-06-05 21:28:39 -0300

    minha cabeeca esta explodindo

--- src/main/java/com/example/demo/controller/AuthenticatiionController.java
+++ src/main/java/com/example/demo/controller/AuthenticatiionController.java
@@ -1,11 +1,9 @@
 package com.example.demo.controller;
 
-import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.web.bind.annotation.RestController;
 
 import com.example.demo.service.AuthenticationService;
 import org.springframework.web.bind.annotation.PostMapping;
-import org.springframework.web.bind.annotation.RequestBody;
 
 
 @RestController
--- src/main/java/com/example/demo/service/PersonDetailService.java
+++ src/main/java/com/example/demo/service/PersonDetailService.java
@@ -1,5 +1,30 @@
 package com.example.demo.service;
 
-public interface PersonDetailService{
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.core.userdetails.UserDetails;
+import org.springframework.security.core.userdetails.UserDetailsService;
+import org.springframework.security.core.userdetails.UsernameNotFoundException;
+
+import com.example.demo.model.PersonAlthenticated;
+import com.example.demo.repository.PersonRepository;
+
+public class PersonDetailService implements UserDetailsService{
+
+    
+    private final PersonRepository personRepository;
+
+    public PersonDetailService(PersonRepository personRepository){
+        this.personRepository = personRepository;
+    }
+
+
+    @Override
+    public UserDetails loadUserByUsername(String username) throws UsernameNotFoundException {
+        
+        return personRepository.findByUsername(username)
+        .map(PersonAlthenticated::new)
+        .orElseThrow(() -> new UsernameNotFoundException("Unsername no found"));
+
+    }
 
 }
